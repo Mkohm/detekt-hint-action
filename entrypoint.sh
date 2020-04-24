bf4ea4a5b54eeebd56651a385beaa0e6957b4976
Marius Kohmann <marius.kohmann@gmail.com>
2020-04-24 21:26:06 +0200

    fix bugs in command

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,13 @@ echo "pwd"
 pwd
 
 # download detekt jar
-curl -sSLO https://github.com/arturbosch/detekt/releases/download/v1.8.0/detekt && chmod a+x detekt
+curl -sSLO https://github.com/detekt/detekt/releases/download/v1.8.0/detekt && chmod a+x detekt
 
 # download detekt-hint jar
-curl -sSL https://oss.sonatype.org/service/local/artifact/maven/redirect?r=releases&g=io.github.mkohm&a=detekt-hint&v=0.1.5&e=jar -o detekt-hint-0.1.5.jar 
+curl -sSLo detekt-hint-0.1.5.jar https://oss.sonatype.org/service/local/artifact/maven/redirect?r=releases&g=io.github.mkohm&a=detekt-hint&v=0.1.5&e=jar
 
 # Run detekt with the detekt-hint plugin, requiring a config file to be set up in the repository
-java -jar detekt.jar --plugin detekt-hint-0.1.5.jar --config config/detekt-hint-config.yml --input / -cp . --report xml:detekt-hint-report.xml --includes '**/src/**/*.kt'
+java -jar detekt --plugins detekt-hint-0.1.5.jar --config config/detekt-hint-config.yml --input / -cp . --report xml:detekt-hint-report.xml --includes '**/src/**/*.kt'
 
 # Install danger-kotlin
 bash <(curl -s https://raw.githubusercontent.com/danger/kotlin/master/scripts/install.sh)
