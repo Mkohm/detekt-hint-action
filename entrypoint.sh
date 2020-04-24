232735e025107c53dc026256a55d840d79d714df
Marius Kohmann <marius.kohmann@gmail.com>
2020-04-24 21:38:38 +0200

    fix input

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ curl -sSLO https://github.com/detekt/detekt/releases/download/v1.8.0/detekt && c
 curl -sSLo detekt-hint-0.1.5.jar https://oss.sonatype.org/service/local/artifact/maven/redirect?r=releases&g=io.github.mkohm&a=detekt-hint&v=0.1.5&e=jar
 
 # Run detekt with the detekt-hint plugin, requiring a config file to be set up in the repository
-java -jar detekt --plugins detekt-hint-0.1.5.jar --config config/detekt-hint-config.yml --input / -cp . --report xml:detekt-hint-report.xml --includes '**/src/**/*.kt'
+java -jar detekt --plugins detekt-hint-0.1.5.jar --config config/detekt-hint-config.yml --input / -cp . --report xml:detekt-hint-report.xml --includes '**/*.kt'
 
 # Install danger-kotlin
 bash <(curl -s https://raw.githubusercontent.com/danger/kotlin/master/scripts/install.sh)
