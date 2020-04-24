e3a0e7392cc63b082275606071e34e3c575495e6
Marius Kohmann <marius.kohmann@gmail.com>
2020-04-24 20:56:04 +0200

    change path

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ curl -sSLO https://github.com/arturbosch/detekt/releases/download/v1.8.0/detekt 
 curl -L https://oss.sonatype.org/service/local/artifact/maven/redirect?r=releases&g=io.github.mkohm&a=detekt-hint&v=0.1.5&e=jar -o detekt-hint-0.1.5.jar 
 
 # Run detekt with the detekt-hint plugin, requiring a config file to be set up in the repository
-java -jar detekt.jar --plugin detekt-hint-0.1.5.jar --config detekt-hint-config.yml --input / -cp . --report xml:detekt-hint-report.xml --includes '**/src/**/*.kt'
+java -jar detekt.jar --plugin detekt-hint-0.1.5.jar --config config/detekt-hint-config.yml --input / -cp . --report xml:detekt-hint-report.xml --includes '**/src/**/*.kt'
 
 # Install danger-kotlin
 bash <(curl -s https://raw.githubusercontent.com/danger/kotlin/master/scripts/install.sh)
