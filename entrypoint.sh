8e4800ecc6b737f4341b5877b56eedc6356d4296
Marius Kohmann <marius.kohmann@gmail.com>
2020-04-24 22:19:59 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,8 @@ curl -sSLO https://github.com/detekt/detekt/releases/download/v1.8.0/detekt && c
 
 # download detekt-hint jar
 curl -sSLo detekt-hint-0.1.5.jar https://oss.sonatype.org/service/local/artifact/maven/redirect?r=releases&g=io.github.mkohm&a=detekt-hint&v=0.1.5&e=jar
+echo "Should show detekt-hint jar:"
+ls
 
 # Run detekt with the detekt-hint plugin, requiring a config file to be set up in the repository
 java -jar detekt --plugins detekt-hint-0.1.5.jar --config config/detekt-hint-config.yml --input / -cp . --report xml:detekt-hint-report.xml --includes '**/*.kt'
