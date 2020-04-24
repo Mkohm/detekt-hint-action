bdd47025a261dba02942e654d4e45c03c2a68ae4
Marius Kohmann <marius.kohmann@gmail.com>
2020-04-24 20:54:43 +0200

    initial commit

---
+++ Dangerfile.df.kts
@@ -0,0 +1,42 @@
+import org.w3c.dom.Document
+import org.w3c.dom.Element
+import org.w3c.dom.NodeList
+import systems.danger.kotlin.Danger
+import systems.danger.kotlin.warn
+import java.io.File
+import javax.xml.parsers.DocumentBuilderFactory
+
+
+val xmlFile: File = File("detekt-hint-report.xml")
+val xmlDoc: Document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(xmlFile)
+xmlDoc.documentElement.normalize()
+
+val danger = Danger(args)
+
+val fileList: NodeList = xmlDoc.getElementsByTagName("file")
+
+for (i in 0 until fileList.length) {
+    var fileNode = fileList.item(i) as Element
+
+
+    val fileName = fileNode.getAttribute("name")
+    println("Filename: $fileName")
+
+    for (k in 0 until fileNode.getElementsByTagName("error").length) {
+        val error = fileNode.getElementsByTagName("error").item(k) as Element
+        println("Error")
+
+        val line = error.getAttribute("line")
+        println("Line: $line")
+        val message = error.getAttribute("message")
+        println("Message: $message")
+
+
+        if (danger.git.modifiedFiles.contains(fileName)) {
+
+            // Only notify about the warning if the file has been modified in this PR
+            warn(message, fileName, line.toInt())
+        }
+    }
+}
+
---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "ls"
+ls
+echo "pwd"
+pwd
+
+# download detekt jar
+curl -sSLO https://github.com/arturbosch/detekt/releases/download/v1.8.0/detekt && chmod a+x detekt
+
+# download detekt-hint jar
+curl -L https://oss.sonatype.org/service/local/artifact/maven/redirect?r=releases&g=io.github.mkohm&a=detekt-hint&v=0.1.5&e=jar -o detekt-hint-0.1.5.jar 
+
+# Run detekt with the detekt-hint plugin, requiring a config file to be set up in the repository
+java -jar detekt.jar --plugin detekt-hint-0.1.5.jar --config detekt-hint-config.yml --input / -cp . --report xml:detekt-hint-report.xml --includes '**/src/**/*.kt'
+
+# Install danger-kotlin
+bash <(curl -s https://raw.githubusercontent.com/danger/kotlin/master/scripts/install.sh)
+source ~/.bash_profile
+
+npx --package danger danger-kotlin ci --dangerfile /Dangerfile.df.kts
